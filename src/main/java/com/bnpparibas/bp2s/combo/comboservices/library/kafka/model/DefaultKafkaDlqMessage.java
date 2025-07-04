5b25a652b9eec0b78b4ead1e4c5686cca2939e0e
Eduardo Morbey <eduardomorbey@gmail.com>
2025-07-05 00:06:47 +0100

    moved back to generator spring

--- src/main/java/com/bnpparibas/bp2s/combo/comboservices/library/kafka/model/DefaultKafkaDlqMessage.java
+++ src/main/java/com/bnpparibas/bp2s/combo/comboservices/library/kafka/model/DefaultKafkaDlqMessage.java
@@ -1,13 +1,11 @@
 package com.bnpparibas.bp2s.combo.comboservices.library.kafka.model;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-@AllArgsConstructor
 public class DefaultKafkaDlqMessage extends GenericKafkaMessage {
     private final String errorMsg;
 }
